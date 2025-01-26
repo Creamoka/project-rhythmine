aa7df5eaa280043bf5fdec008e214468e3455c21
Creamoka <aryamaulanayusuf467@gmail.com>
2025-01-26 15:57:13 +0700

    Add new audio tracks and update paths in SongSelectionMenu for better resource management

--- src/main/java/com/example/rhytmine/SongSelectionMenu.java
+++ src/main/java/com/example/rhytmine/SongSelectionMenu.java
@@ -1,8 +1,12 @@
 package com.example.rhytmine;
 
-import javax.swing.*;
-import java.awt.*;
+import java.awt.GridLayout;
 import java.util.List;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 class SongSelectionMenu extends JFrame {
     public SongSelectionMenu() {
@@ -23,17 +27,17 @@ class SongSelectionMenu extends JFrame {
         };
         String[] osuPaths = {
             "src\\main\\resources\\song\\Elevator (Nightcore Mix)\\Eliminate - Elevator (Nightcore Mix) (Ainer) [Levitation].osu",
-            "resources/song/Aarena (Knock2 Remix)/ISOxo - Aarena (Knock2 Remix) (Ainer) [Rrealm].osu",
-            "resources/song/Mean To Be (Nightcore Mix)/ZOOTAH & GA-SUUU & Petboy - Meant To Be (Nightcore Mix) (Ainer) [Inevitable].osu",
-            "resources/song/Gotta Get Thru This (Jauz Remix)/Daniel Bedingfield - Gotta Get Thru This (Jauz RetroFuture Remix) (Nightcore Mix) (Claren) [Getting Thru These SVs].osu",
-            "resources/song/Sugar Rush (Nightcore Mix)/Teknicolor - Sugar Rush (Nightcore Mix) (Andere) [Mir4cle (SV)].osu"
+            "src\\main\\resources\\song\\Aarena (Knock2 Remix)\\ISOxo - Aarena (Knock2 Remix) (Ainer) [Rrealm].osu",
+            "src\\main\\resources\\song\\Mean To Be (Nightcore Mix)\\ZOOTAH & GA-SUUU & Petboy - Meant To Be (Nightcore Mix) (Ainer) [Inevitable].osu",
+            "src\\main\\resources\\song\\Gotta Get Thru This (Jauz Retro Future Remix)\\Daniel Bedingfield - Gotta Get Thru This (Jauz RetroFuture Remix) (Nightcore Mix) (Claren) [Getting Thru These SVs].osu",
+            "src\\main\\resources\\song\\Sugar Rush (Nightcore Mix)\\Teknicolor - Sugar Rush (Nightcore Mix) (Andere) [Mir4cle (SV)].osu"
         };
         String[] audioPaths = {
             "src\\main\\resources\\song\\Elevator (Nightcore Mix)\\audio.wav",
-            "resources/song/Aarena (Knock2 Remix)/audio.ogg",
-            "resources/song/Mean To Be (Nightcore Mix)/audio.mp3",
-            "resources/song/Gotta Get Thru This (Jauz Remix)/audio.mp3",
-            "resources/song/Sugar Rush (Nightcore Mix)/audio.mp3"
+            "src\\main\\resources\\song\\Aarena (Knock2 Remix)\\audio.wav",
+            "src\\main\\resources\\song\\Mean To Be (Nightcore Mix)\\audio.wav",
+            "src\\main\\resources\\song\\Gotta Get Thru This (Jauz Retro Future Remix)\\audio.wav",
+            "src\\main\\resources\\song\\Sugar Rush (Nightcore Mix)\\audio.wav"
         };
 
         for (int i = 0; i < songNames.length; i++) {
